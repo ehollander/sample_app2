b27eb869bd50da03d31dc8aa8754368f79752f70
Eric Hollander <ehollander@bauerpublishing.com>
2013-05-20 19:29:42 -0400

    Create - Edit - Delete Magazines Complete

--- app/controllers/magazines_controller.rb
+++ app/controllers/magazines_controller.rb
@@ -14,6 +14,9 @@ class MagazinesController < ApplicationController
   end
 
   def destroy
+  	Magazine.find(params[:id]).destroy
+    	flash[:success] = "Magazine destroyed."
+    	redirect_to magazines_url
   end
 
   def index
@@ -23,4 +26,18 @@ class MagazinesController < ApplicationController
   def show
 
   end
+
+  def edit
+  		@magazine = Magazine.find(params[:id])
+  	end	
+
+  def update
+	    @magazine = Magazine.find(params[:id])
+	    if @magazine.update_attributes(params[:magazine])
+	      flash[:success] = "Magazine updated"
+	      redirect_to magazines_url
+	    else
+	      render 'edit'
+	    end
+	end	
 end
