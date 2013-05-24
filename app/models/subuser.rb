afd63ae1b4b3b3f375e8f9e47ec0626719219c3b
Eric Hollander <ehollander@bauerpublishing.com>
2013-05-23 21:59:38 -0400

    fucked up

--- app/models/subuser.rb
+++ app/models/subuser.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: subusers
+#
+#  id         :integer          not null, primary key
+#  subname    :string(255)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Subuser < ActiveRecord::Base
   attr_accessible :subname, :user_id
   belongs_to :user
